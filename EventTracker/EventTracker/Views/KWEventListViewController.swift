5650906aadc1e34fa08d51ffc7c00b1bac01bf72
Ankita Kalangutkar <ankitakalangutkar@Ankitas-MacBook-Pro-2.local>
2016-10-16 10:44:21 +0530

    Added a tableview controller

---
+++ EventTracker/EventTracker/Views/KWEventListViewController.swift
@@ -0,0 +1,13 @@
+//
+//  KWEventListViewController.swift
+//  EventTracker
+//
+//  Created by Ankita Kalangutkar on 10/16/16.
+//  Copyright © 2016 Ankita Kalangutkar. All rights reserved.
+//
+
+import UIKit
+
+class KWEventListViewController: UITableViewController {
+
+}
