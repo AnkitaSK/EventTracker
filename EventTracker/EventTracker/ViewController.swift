2c52687ad6f723fa45750b658f6ee5e4410c1ca5
Ankita Kalangutkar <ankitakalangutkar@Ankitas-MacBook-Pro-2.local>
2016-10-16 10:10:36 +0530

    Initial project setup

---
+++ EventTracker/EventTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EventTracker
+//
+//  Created by Ankita Kalangutkar on 10/16/16.
+//  Copyright © 2016 Ankita Kalangutkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
