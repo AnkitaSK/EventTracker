6659e43f18efdc54af0776073a032b2fe5a51ad4
Ankita Kalangutkar <ankitakalangutkar@Ankitas-MacBook-Pro-2.local>
2016-10-17 21:06:57 +0530

    Added content to the detail screen

--- EventTracker/EventTracker/Views/KWDetailViewController.swift
+++ EventTracker/EventTracker/Views/KWDetailViewController.swift
@@ -11,6 +11,24 @@ import CoreData
 
 class KWDetailViewController: UIViewController {
     var eventData:NSManagedObject?
+    @IBOutlet var eventNameLabel: UILabel!
+    @IBOutlet var eventPlaceLabel: UILabel!
+    @IBOutlet var eventTypeLabel: UILabel!
+    
+    @IBOutlet var eventTrackButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        eventNameLabel.text = eventData!.valueForKey("eventName") as? String
+        eventPlaceLabel.text = eventData!.valueForKey("eventPlace") as? String
+        eventTypeLabel.text = eventData!.valueForKey("eventEntryType") as? String
+        
+        eventTrackButton.backgroundColor = UIColor.redColor()
+        eventTrackButton.layer.cornerRadius = 5
+        eventTrackButton.layer.borderWidth = 1
+        eventTrackButton.layer.borderColor = UIColor.blackColor().CGColor
+    }
+    
     @IBAction func trackEventButtonclicked(sender: UIButton) {
         
         DatabaseManager.sharedManager.saveEventForRelationship(eventData!) { (success) -> Void in
